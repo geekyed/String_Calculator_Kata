fb029c4ef646f0250dc2fe6f04ab2c1878f57091
Matthew Swarbrick <matt_swarbrick@hotmail.com>
2014-09-11 09:58:41 +0100

    Added initial project structure

---
+++ StringCalculator/StringCalculator/StringCalculatorTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace StringCalculator
+{
+    [TestFixture]
+    public class StringCalculatorTests
+    {
+        [Test]
+        public void AnEmptyStringReturnsZero()
+        {
+
+        }
+    }
+}
