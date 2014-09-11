29572370bb590886f021c5bbc916ccd0bcd53a1b
Matthew Swarbrick <matt_swarbrick@hotmail.com>
2014-09-11 11:36:20 +0100

    Fixed failing tests around defining a new single char delimiter

--- StringCalculator/StringCalculator/StringCalculator.cs
+++ StringCalculator/StringCalculator/StringCalculator.cs
@@ -1,13 +1,12 @@
 ﻿using System;
 using System.Linq;
+using System.Collections.Generic;
 
 namespace StringCalculator
 {
     public class StringCalculator
     {
-        public StringCalculator()
-        {
-        }
+        private List<string> Delimiters = new List<string> { ",", "\n"};
 
         public int Calculate(string input)
         {
@@ -16,7 +15,14 @@ namespace StringCalculator
                 return 0;
             }
 
-            var values = input.Split(new [] { ",", "\n"}, StringSplitOptions.RemoveEmptyEntries);
+            if(input.StartsWith("//"))
+            {
+                Delimiters.Add(input[2].ToString());
+
+                input = input.TrimStart('/');
+            }
+
+            var values = input.Split(Delimiters.ToArray(), StringSplitOptions.RemoveEmptyEntries);
 
             var valuesAsInt = values.Select(x => Int32.Parse(x));
 
